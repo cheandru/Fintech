451bf39510ca8d87970061ef087b9822b5224344
cheandru <cheandru@gmail.com>
2018-11-19 19:29:01 +0530

    Add files via upload

---
+++ src/app/fileupload/fileupload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fileupload',
+  templateUrl: './fileupload.component.html',
+  styleUrls: ['./fileupload.component.css']
+})
+export class FileuploadComponent {
+  title = 'fileupload';
+  
+}  
